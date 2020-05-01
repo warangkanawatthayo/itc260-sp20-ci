940fe8b16ec87363ef3f8956a3042e43c536b5e1
warangkana watthayo <watthayo.warangkana@seattlecentral.edu>
2020-04-30 22:29:32 -0700

    Adds feedback and slug to create()
    Now, when a new item is added, the slug is passed to the view and we see the new items , with feedback .

--- sp20/application/controllers/News.php
+++ sp20/application/controllers/News.php
@@ -15,10 +15,8 @@ class News extends CI_Controller {
         {
                 $data['news'] = $this->news_model->get_news();
                 $data['title'] = 'News archive';
-        
-                //$this->load->view('templates/header', $data);
                 $this->load->view('news/index', $data);
-                //$this->load->view('templates/footer', $data);
+                
         }
  
         public function view($slug = NULL)
@@ -30,11 +28,9 @@ class News extends CI_Controller {
                         show_404();
                 }
         
-                $data['title'] = $data['news_item']['title'];
-        
-                $this->load->view('templates/header', $data);
+                $data['title'] = $data['news_item']['title'];   
                 $this->load->view('news/view', $data);
-                $this->load->view('templates/footer', $data);
+               
         }
  
         public function create()
@@ -49,17 +45,24 @@ class News extends CI_Controller {
  
                 if ($this->form_validation->run() === FALSE)
                 {
-                        $this->load->view('templates/header', $data);
+                        
                         $this->load->view('news/create', $data);
-                        $this->load->view('templates/footer', $data);
+                       
  
                 }
                 else
                 {
-                        $this->news_model->set_news();
-                        $this->load->view('templates/header', $data);
-                        $this->load->view('news/success', $data);
-                        $this->load->view('templates/footer', $data);
+                        //$this->news_model->set_news();
+                        //$this->load->view('news/success', $data);
+                       
+                    $slug = $this->news_model->set_news();
+                    if($slug!==false){
+                            feedback('Data entered successfully','info');
+                            redirect('news/view/' . $slug);      
+                    }else{
+                            feedback('Data not entered','error');
+                            redirect('news/create');
+                    }
                 }
         }
 }
